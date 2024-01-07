b2688c0800e90f7420b2d8651b09a240588cf4b8
Teuku Akhdansyah <akhdansyah@gmail.com>
2024-01-07 16:46:54 +0700

    Fix values error

--- src/app.py
+++ src/app.py
@@ -8,13 +8,13 @@ st.title("Credit Card Fraud Detector")
 
 # Input form for user to enter transaction details
 st.header("Enter Transaction Details")
-step = st.number_input("Step", min_value=1, value=1)
+step = st.number_input("Step", min_value=1, value=743)
 trans_type = st.selectbox("Transaction Type", ["CASH_IN", "CASH_OUT", "DEBIT", "PAYMENT", "TRANSFER"])
-amount = st.number_input("Amount", min_value=0.0, value=100.0)
-oldbalanceOrg = st.number_input("Old Balance Origin", min_value=0.0, value=500.0)
-newbalanceOrig = st.number_input("New Balance Origin", min_value=0.0, value=450.0)
-oldbalanceDest = st.number_input("Old Balance Destination", min_value=0.0, value=1000.0)
-newbalanceDest = st.number_input("New Balance Destination", min_value=0.0, value=950.0)
+amount = st.number_input("Amount", min_value=0.0, value=258355.42)
+oldbalanceOrg = st.number_input("Old Balance Origin", min_value=0.0, value=258355.42)
+newbalanceOrig = st.number_input("New Balance Origin", min_value=0.0, value=0.00)
+oldbalanceDest = st.number_input("Old Balance Destination", min_value=0.0, value=25176.67)
+newbalanceDest = st.number_input("New Balance Destination", min_value=0.0, value=283532.09)
 
 # Make prediction button
 if st.button("Make Prediction"):
